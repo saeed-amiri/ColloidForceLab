8506df1b17b6040864be07ef571083f1c8045758
Saeed Amiri <samiri@uni-bremen.de>
2024-06-10 10:22:58 +0200

    set the constant values

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+"""
+Constant values in the dLVO model
+"""
+# Define constants
+A: float = 1e-21  # Hamaker constant in Joules
+R_NP: float = 30e-9  # Radius of nanoparticles in meters
+EPSILON = 78.8  # Dielectric constant of water
+EPSILON_0 = 8.854e-12  # Permittivity of free space in F/m
+PSI_0 = 172e-3  # Surface potential in Volts
+KAPPA = 1e9  # Inverse Debye length in 1/m
